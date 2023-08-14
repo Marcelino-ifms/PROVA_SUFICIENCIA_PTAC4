bfd2c9060b5d00f22cf549bf534a207385bfe105
MarcelinoGarcia-NA <marcelinovitorgarcia@gmail.com>
2023-08-14 13:47:05 -0300

    prova suficiência

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,16 +1,24 @@
 'use server'
-const urlApi = '';
+const urlApi = 'https://teste-ptas3.vercel.app';
 
 const getUserAuthenticated = async (user) => {
-
+    const response = await fetch(urlApi + '/user/authenticated', {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: (JSON.stringify(user))
+    });
+    const data = await response?.json();
+    return data;
 }
 
 const getUser = async () => {
-
+    const response = await fetch(urlApi + '/user', {
+        cache: "no-cache"
+    });
+    const data = await response?.json();
+    return data;
 }
 
-const postUser = async () => {
+export { getUserAuthenticated, getUser};
 
-}
 
-export { getUser, getUserAuthenticated, postUser };
--- src/middleware.js
+++ src/middleware.js
@@ -6,21 +6,22 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
 
-    const urlLogin = new URL('/',request.url);
-    const urlDashboard = new URL('/pages/dashboard',request.url);
+    const urlLogin = new URL('/', request.url);
     const isTokenValidated = validateToken(token);
 
-    if(!isTokenValidated || !token){
-        if(request.nextUrl.pathname==='/pages/dashboard'){
+    if (!isTokenValidated || !token) {
+
+        if (request.nextUrl.pathname === '/pages/dashboard'
+
+            || request.nextUrl.pathname === '/pages/register') {
+
             return NextResponse.redirect(urlLogin);
         }
     }
-    if(request.nextUrl.pathname==='/'){
-        return NextResponse.redirect(urlDashboard);
-    }
+    
     NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard']
+    matcher: ['/', '/pages/dashboard','/pages/register']
 };
 
